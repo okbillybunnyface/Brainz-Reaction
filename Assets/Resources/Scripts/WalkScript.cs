3b2ce71b8919c8b8c38535157c62ca9e9843d1ea
Caroline Gingles <okbillybunnyface@gmail.com>
2014-11-21 22:04:21 -0600

    -Changed movement to be force based

--- Assets/Resources/Scripts/WalkScript.cs
+++ Assets/Resources/Scripts/WalkScript.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(CharacterController))]
 public class WalkScript : MonoBehaviour 
 {
-    public float speed = 1f;
+    public float maxSpeed = 1f;
+    private float speed;
+    public float moveAccel = 1f;
     public float turnSpeed = 15f;
     private GameObject target;
     private bool seeking = false;
 
+    void Start()
+    {
+        StartCoroutine(Walking());
+    }
 
     void FixedUpdate()
     {
@@ -20,16 +27,31 @@ public class WalkScript : MonoBehaviour
         if (!seeking)
         {
             seeking = true;
-            StartCoroutine(Seek());
+            StartCoroutine(Seeking());
         }
     }
 
     private void Walk(float speed)
     {
-        transform.position = transform.position + transform.right * Time.deltaTime * speed;
+        this.speed = speed;
+        if (speed > maxSpeed) speed = maxSpeed;
     }
 
-    IEnumerator Seek()
+    IEnumerator Walking()
+    {
+        while (true)
+        {
+            Vector2 force1 = speed * transform.right * moveAccel * (1 - Vector2.Dot(transform.right, rigidbody2D.velocity) / maxSpeed);
+            Vector2 force2 = transform.up * moveAccel * (0 - Vector2.Dot(transform.up, rigidbody2D.velocity) / maxSpeed);
+            rigidbody2D.AddForce(force1 + force2);
+
+            speed = 0;
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    IEnumerator Seeking()
     {
         while (seeking)
         {
