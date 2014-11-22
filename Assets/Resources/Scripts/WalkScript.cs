a25eee264204a429b74c975f0c6e618ce324f163
Caroline Gingles <okbillybunnyface@gmail.com>
2014-11-21 20:57:36 -0600

    -Things can now seek a target

---
+++ Assets/Resources/Scripts/WalkScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkScript : MonoBehaviour 
+{
+    public float speed = 1f;
+    public float turnSpeed = 15f;
+    private GameObject target;
+    private bool seeking = false;
+
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public void SeekTarget(GameObject target)
+    {
+        this.target = target;
+        if (!seeking)
+        {
+            seeking = true;
+            StartCoroutine(Seek());
+        }
+    }
+
+    private void Walk(float speed)
+    {
+        transform.position = transform.position + transform.right * Time.deltaTime * speed;
+    }
+
+    IEnumerator Seek()
+    {
+        while (seeking)
+        {
+            Vector3 toTarget = target.transform.position - transform.position;
+            transform.right = Vector3.Lerp(transform.right, toTarget, Time.deltaTime * turnSpeed);
+
+            Walk(speed);
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/ZombieScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (WalkScript))]
+public class ZombieScript : MonoBehaviour 
+{
+    private WalkScript walkScript;
+    private GameObject target;
+
+	// Use this for initialization
+	void Start () 
+    {
+        walkScript = this.GetComponent<WalkScript>();
+        target = GameObject.FindGameObjectWithTag("Human");
+        walkScript.SeekTarget(target);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
