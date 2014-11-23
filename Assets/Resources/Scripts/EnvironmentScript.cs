789c0ff1475d35ec56b399072381fdbbacf491e2
Sammy Hamdan <sammy.hamdan@gmail.com>
2014-11-23 12:47:38 -0600

    -First human zombie selection

--- Assets/Resources/Scripts/EnvironmentScript.cs
+++ Assets/Resources/Scripts/EnvironmentScript.cs
@@ -7,7 +7,7 @@ public class EnvironmentScript : MonoBehaviour {
 	public bool paused = false;
 	public float pauseTime = 5f;
 	public static GameObject currentZombie, currentTarget;
-	private static bool zombieSelectedFirst = false;
+	private static bool zombieSelectedFirst = false, firstZombieSpawned = false;
     public static System.Random random = new System.Random();
 
 
@@ -31,28 +31,36 @@ public class EnvironmentScript : MonoBehaviour {
 	//Highlights the zombie and his current target
 	public static void showSelectedTargets(GameObject zombie, GameObject target)
 	{
-		if (currentZombie != null) currentZombie.SendMessage("setCircleActive", false, SendMessageOptions.DontRequireReceiver);
-		if (currentTarget != null) currentTarget.SendMessage("setCircleActive", false, SendMessageOptions.DontRequireReceiver);
 
-		currentZombie = zombie;
-		currentTarget = target;
-		zombieSelectedFirst = true; //verifying a human can't be selected w/o a zombie being first
+			if (currentZombie != null) currentZombie.SendMessage("setCircleActive", false, SendMessageOptions.DontRequireReceiver);
+			if (currentTarget != null) currentTarget.SendMessage("setCircleActive", false, SendMessageOptions.DontRequireReceiver);
 
-		currentZombie.SendMessage("setCircleActive", true, SendMessageOptions.DontRequireReceiver);
-		target.SendMessage("setCircleActive", true, SendMessageOptions.DontRequireReceiver);
+			currentZombie = zombie;
+			currentTarget = target;
+			zombieSelectedFirst = true; //verifying a human can't be selected w/o a zombie being first
 
+			currentZombie.SendMessage("setCircleActive", true, SendMessageOptions.DontRequireReceiver);
+				target.SendMessage("setCircleActive", true, SendMessageOptions.DontRequireReceiver);
 	}
 
 	//Highlights the selected human
 	public static void setHumanMarked(GameObject human)
 	{
-		if (zombieSelectedFirst)
+		if(!firstZombieSpawned)
 		{
-			currentTarget.SendMessage("setCircleActive", false, SendMessageOptions.DontRequireReceiver); //turn off old human
-			human.SendMessage("setCircleActive", true, SendMessageOptions.DontRequireReceiver); //turn on new human
-			setZombieTarget(human);
+			if(human != null) human.SendMessage("Zombify", SendMessageOptions.DontRequireReceiver);
+			firstZombieSpawned = true;
 		}
-		else Debug.Log("You must pick a zombie first!");
+		else
+		{
+			if (zombieSelectedFirst)
+			{
+				currentTarget.SendMessage("setCircleActive", false, SendMessageOptions.DontRequireReceiver); //turn off old human
+				human.SendMessage("setCircleActive", true, SendMessageOptions.DontRequireReceiver); //turn on new human
+				setZombieTarget(human);
+			}
+			else Debug.Log("You must pick a zombie first!");
+		}
 	}
 
 	//Sets the zombies target
